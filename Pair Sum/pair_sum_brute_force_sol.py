dfc320b5811bea136c84ff1fcacbae7b50028927
Ebad <146643721+smebad@users.noreply.github.com>
2024-12-16 17:46:28 +0500

    pair_sum_brute_force_sol.py

--- Pair Sum/pair_sum_brute_force_sol.py
+++ Pair Sum/pair_sum_brute_force_sol.py
@@ -1,11 +1,7 @@
 # pair sum problem
-# Write a function, pair_sum, that takes in a list and a target sum as arguments. The function should return a tuple containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
-
-# Be sure to return the indices, not the elements themselves.
-
-# There is guaranteed to be one such pair that sums to the target.
-
+# Write a function, pair_sum, that takes in a list and 
 # Brute Force Solution
+
 def pair_sum(numbers, target_sum):
   for i in range(0, len(numbers)):
     for j in range(i + 1, len(numbers)):
