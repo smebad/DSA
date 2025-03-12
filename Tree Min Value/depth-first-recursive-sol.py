ec7dff6f045e1f79521ae439908f9c5262db303b
smebad <mohammadebad1@hotmail.com>
2025-03-12 22:45:03 +0500

    tree min value depth first (recursive) solution

---
+++ Tree Min Value/depth-first-recursive-sol.py
@@ -0,0 +1,105 @@
+# tree min value
+# Write a function, tree_min_value, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
+
+# You may assume that the input tree is non-empty.
+
+
+# Depth First Solution (Recursive):
+class Node:
+   def __init__(self, val):
+     self.val = val
+     self.left = None
+     self.right = None
+
+def tree_min_value(root):
+  if root is None:
+    return float("inf")
+  smallest_left_value = tree_min_value(root.left)
+  smallest_right_value = tree_min_value(root.right)
+  return min(root.val, smallest_left_value, smallest_right_value)
+
+
+# Time Complexity:
+# n = number of nodes
+# Time: O(n)
+# Space: O(n)
+
+
+# Test Cases:
+# Test Case 1
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+print(tree_min_value(a)) # -> -2
+
+# Test Case 2
+a = Node(5)
+b = Node(11)
+c = Node(3)
+d = Node(4)
+e = Node(14)
+f = Node(12)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       5
+#    /    \
+#   11     3
+#  / \      \
+# 4   14     12
+
+print(tree_min_value(a)) # -> 3
+
+# Test Case 3
+a = Node(-1)
+b = Node(-6)
+c = Node(-5)
+d = Node(-3)
+e = Node(-4)
+f = Node(-13)
+g = Node(-2)
+h = Node(-2)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#        -1
+#      /   \
+#    -6    -5
+#   /  \     \
+# -3   -4   -13
+#     /       \
+#    -2       -2
+
+print(tree_min_value(a)) # -> -13
+
+# Test Case 4
+a = Node(42)
+
+#        42
+
+print(tree_min_value(a)) # -> 42
