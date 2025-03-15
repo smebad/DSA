9a993629bd5483756a16d12c6afd72fe918588cc
smebad <mohammadebad1@hotmail.com>
2025-03-15 23:39:30 +0500

    Has Path - depth first solution

---
+++ Has Path/depth_first_sol.py
@@ -0,0 +1,85 @@
+# has path
+# Write a function, has_path, that takes in a dictionary representing the adjacency list of a directed acyclic graph and two nodes (src, dst). The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
+
+# Depth First Solution:
+def has_path(graph, src, dst):
+  if src == dst:
+    return True
+  
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst) == True:
+      return True
+    
+  return False
+
+# Time Complexity:
+# n = number of nodes
+# e = number edges
+# Time: O(e)
+# Space: O(n)
+
+
+# Test Cases:
+# Test Case 1:
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+print(has_path(graph, 'f', 'k')) # True
+
+# Test Case 2:
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+print(has_path(graph, 'f', 'j')) # False
+
+# Test Case 3:
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+print(has_path(graph, 'i', 'h')) # True
+
+
+# Test Case 4:
+graph = {
+  'v': ['x', 'w'],
+  'w': [],
+  'x': [],
+  'y': ['z'],
+  'z': [],  
+}
+
+print(has_path(graph, 'v', 'w')) # True
+
+
+# Test Case 5:
+graph = {
+  'v': ['x', 'w'],
+  'w': [],
+  'x': [],
+  'y': ['z'],
+  'z': [],  
+}
+
+print(has_path(graph, 'v', 'z')) # False
+
+
+
+
