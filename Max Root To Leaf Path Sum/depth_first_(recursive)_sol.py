32cdfd86527798b979fed8a187a4dfd0f162731b
Ebad <146643721+smebad@users.noreply.github.com>
2025-03-18 23:58:27 +0500

    depth_first_(recursive)_sol.py

--- Max Root To Leaf Path Sum/depth_first_(recursive)_sol.py
+++ Max Root To Leaf Path Sum/depth_first_(recursive)_sol.py
@@ -1,9 +1,3 @@
-# max root to leaf path sum
-# Write a function, max_path_sum, that takes in the root of a binary tree that contains number values. The function should return the maximum sum of any root to leaf path within the tree.
-
-# You may assume that the input tree is non-empty.
-
-
 # Depth First Solution (Recursive):
 class Node:
    def __init__(self, val):
