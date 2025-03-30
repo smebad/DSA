cafb10d6f93d740d3f2694019c32386c6fa36ed4
Ebad <146643721+smebad@users.noreply.github.com>
2025-03-31 00:58:13 +0500

    Add files via upload
    Leetcode Blind75 - Valid Anagrams hashmap solution

---
+++ 02. Valid Anagram/hashmap_solution.py
@@ -0,0 +1,56 @@
+# Valid Anagram
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+
+# Sorting Solution:
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
+    
+# Time and Space Complexity: 
+# O(n log n + m log m) for sorting both strings, where n and m are the lengths of the two strings.
+# O(1) or O(n+m) for the space complexity, depending on the sorting algorithm used.
+
+# Test Cases:
+
+# Test Case 1
+s = "carrace"
+t = "racecar"
+solution = Solution()
+print(solution.isAnagram(s, t))  # Output: False
+
+# Test Case 2
+s = "hello"
+t = "world"
+solution = Solution()
+print(solution.isAnagram(s, t))  # Output: False
