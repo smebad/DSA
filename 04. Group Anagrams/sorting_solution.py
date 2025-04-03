bf02fed809d487bd6cca90aee5bac717d088fd7e
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-03 23:35:35 +0500

    LeetCode Blind 75 - Group Anagrams sorting sol

---
+++ 04. Group Anagrams/sorting_solution.py
@@ -0,0 +1,58 @@
+# Group Anagrams
+# Solved 
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+# Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+# Example 2:
+
+# Input: strs = ["x"]
+
+# Output: [["x"]]
+# Example 3:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+# Constraints:
+
+# 1 <= strs.length <= 1000.
+# 0 <= strs[i].length <= 100
+# strs[i] is made up of lowercase English letters.
+
+
+# Sorting solution:
+from typing import List
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+    
+# Time complexity: O(m * nlogn), where m is the number of strings and n is the maximum length of a string. This is because we sort each string, which takes O(nlogn) time, and we do this for m strings.
+# Space complexity: O(m * n), where m is the number of strings and n is the maximum length of a string. This is because we store all the strings in the result list and the dictionary.
+
+
+# Test Cases
+# Test case 1
+strs = ["act","pots","tops","cat","stop","hat"]
+solution = Solution()
+print(solution.groupAnagrams(strs))  # Expected output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+# Test case 2
+strs = ["x"]
+solution = Solution()
+print(solution.groupAnagrams(strs))  # Expected output: [["x"]]
+
+# Test case 3
+strs = [""]
+solution = Solution()
+print(solution.groupAnagrams(strs))  # Expected output: [[""]]
