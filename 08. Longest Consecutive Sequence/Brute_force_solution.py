e8848648b058d153ad59b11a09cd1954f62e65be
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-08 23:05:24 +0500

    Leetcode - longest consecutive seq brute force sol

---
+++ 08. Longest Consecutive Sequence/Brute_force_solution.py
@@ -0,0 +1,61 @@
+# Longest Consecutive Sequence
+# Medium
+# Topics
+# Companies
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+# You must write an algorithm that runs in O(n) time.
+
+ 
+
+# Example 1:
+
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+# Example 2:
+
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+# Example 3:
+
+# Input: nums = [1,0,1,2]
+# Output: 3
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+
+# Brute Force Solution:
+from typing import List
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0
+        store = set(nums)
+
+        for num in nums:
+            streak, curr = 0, num
+            while curr in store:
+                streak += 1
+                curr += 1
+            res = max(res, streak)
+        return res
+    
+
+# Time Complexity: O(n^2) in the worst case, where n is the length of the input list nums.
+# Space Complexity: O(n) for the set store.
+
+# Test Cases:
+# Test Case 1:
+nums = [100,4,200,1,3,2]
+print(Solution().longestConsecutive(nums)) # Output: 4
+
+# Test Case 2:
+nums = [0,3,7,2,5,8,4,6,0,1]
+print(Solution().longestConsecutive(nums)) # Output: 9
+
+# Test Case 3:
+nums = [1,0,1,2]
+print(Solution().longestConsecutive(nums)) # Output: 3
