468c368fa3c61652da3d6aa981e1842f97356a5a
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-09 23:06:22 +0500

    Leetcode - Valid palindrome (reverse string sol..)

---
+++ 09. Valid Palindrome/reverse_string_sol.py
@@ -0,0 +1,54 @@
+# Valid Palindrome
+# Easy
+# Topics
+# Companies
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+# reverse string solution:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
+    
+# Time complexity: O(n), where n is the length of the string s.
+# Space complexity: O(n), where n is the length of the string s.
+
+# Test Cases:
+# Test Case 1
+print(Solution().isPalindrome('A man, a plan, a canal: Panama')) # True
+
+# Test Case 2
+print(Solution().isPalindrome('race a car')) # False
+
+# Test Case 3
+print(Solution().isPalindrome(' ')) # True
