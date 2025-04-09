de478ab941cf7fa0f329b0bf2873a4794317bd2d
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-09 23:33:10 +0500

    Valid Palindrome Two pointers solution (recommended)

---
+++ 09. Valid Palindrome/Two_pointers_sol.py
@@ -0,0 +1,65 @@
+# Valid Palindrome
+# Easy
+# Topics
+# Companies
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+
+# Two Pointers solution:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+    
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
+    
+# Time complexity: O(n), where n is the length of the string s.
+# Space complexity: O(n), where n is the length of the string s.
+
+# Test Cases:
+# Test Case 1
+print(Solution().isPalindrome('A man, a plan, a canal: Panama')) # True
+
+# Test Case 2
+print(Solution().isPalindrome('race a car')) # False
+
+# Test Case 3
+print(Solution().isPalindrome(' ')) # True
