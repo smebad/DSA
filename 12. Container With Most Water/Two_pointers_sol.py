3ad446765ebadc35b4d4db382084f80c46a5292a
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-13 23:38:13 +0500

    Two pointers (optimal) solution

---
+++ 12. Container With Most Water/Two_pointers_sol.py
@@ -0,0 +1,65 @@
+# Container With Most Water
+# Medium
+# Topics
+# Companies
+# Hint
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+# Notice that you may not slant the container.
+
+ 
+
+# Example 1:
+
+
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Example 2:
+
+# Input: height = [1,1]
+# Output: 1
+ 
+
+# Constraints:
+
+# n == height.length
+# 2 <= n <= 105
+# 0 <= height[i] <= 104
+
+
+# Two Pointers solution:
+from typing import List
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+    
+# Time 
+
+
+# Test Cases:
+# Test Case 1:
+heights = [1,8,6,2,5,4,8,3,7]
+print(Solution().maxArea(heights)) # Expected Output: 49
+
+# Test Case 2:
+heights = [1,1]
+print(Solution().maxArea(heights)) # Expected Output: 1
+
+# Test Case 3:
+heights = [4,3,2,1,4]
+print(Solution().maxArea(heights)) # Expected Output: 16
