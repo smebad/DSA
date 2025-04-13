b86d8015f885b495341f92d58b942bb4c64f9f68
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-13 23:44:26 +0500

    Leetcode - Container with most water (two pointers solution)
    the most optimal one

--- 12. Container With Most Water/Two_pointers_sol.py
+++ 12. Container With Most Water/Two_pointers_sol.py
@@ -1,37 +1,4 @@
 # Container With Most Water
-# Medium
-# Topics
-# Companies
-# Hint
-# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-
-# Find two lines that together with the x-axis form a container, such that the container contains the most water.
-
-# Return the maximum amount of water a container can store.
-
-# Notice that you may not slant the container.
-
- 
-
-# Example 1:
-
-
-# Input: height = [1,8,6,2,5,4,8,3,7]
-# Output: 49
-# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-# Example 2:
-
-# Input: height = [1,1]
-# Output: 1
- 
-
-# Constraints:
-
-# n == height.length
-# 2 <= n <= 105
-# 0 <= height[i] <= 104
-
-
 # Two Pointers solution:
 from typing import List
 class Solution:
@@ -48,7 +15,9 @@ class Solution:
                 r -= 1
         return res
     
-# Time 
+# Time complexity: O(n) which means the time taken to run the code increases linearly with the increase in the input size.
+# Space complexity: O(1) which means the space taken by the code is constant and does not depend on the input size.
+# Two pointers solution is optimal for large inputs as it takes O(n) time complexity.
 
 
 # Test Cases:
