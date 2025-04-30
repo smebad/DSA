731eceff5b31f49ebfb7dd85a51834472c9801b7
Ebad <146643721+smebad@users.noreply.github.com>
2025-04-30 23:50:29 +0500

    Remove Nth Node From End of List - brute force solution

--- 27. Remove Nth Node From End of List/Brute_force_sol.py
+++ 27. Remove Nth Node From End of List/Brute_force_sol.py
@@ -1,22 +1,6 @@
 # Remove Nth Node From End of List
-# Given the head of a linked list, remove the nth node from the end of the list and return its head.
-
- 
-
-# Example 1:
-# Input: head = [1,2,3,4,5], n = 2
-# Output: [1,2,3,5]
-
-# Example 2:
-# Input: head = [1], n = 1
-# Output: []
-
-# Example 3:
-# Input: head = [1,2], n = 1
-# Output: [1]
-
-
 # Brute force solution:
+
 from typing import Optional
 # Definition for singly-linked list.
 class ListNode:
