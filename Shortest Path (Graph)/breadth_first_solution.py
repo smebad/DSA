9ad2121ad8bd125177a419a6edb0cac16d2775c5
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-16 21:29:12 +0500

    breadth_first_solution.py

--- Shortest Path (Graph)/breadth_first_solution.py
+++ Shortest Path (Graph)/breadth_first_solution.py
@@ -1,8 +1,6 @@
 # shortest path
-# Write a function, shortest_path, that takes in a list of edges for an undirected graph and two nodes (node_A, node_B). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1. You can assume that A and B exist as nodes in the graph.
-
-
 # Breadth First Solution:
+
 from collections import deque
 
 def shortest_path(edges, node_A, node_B):
