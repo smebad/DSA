5431a7285d7b795efa36e89e617ae5bf7ad05862
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-16 21:28:20 +0500

    iterative_sol.py

--- Reverse list/iterative_sol.py
+++ Reverse list/iterative_sol.py
@@ -1,7 +1,6 @@
 # reverse list
-# Write a function, reverse_list, that takes in the head of a linked list as an argument. The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
-
 # iterative solution
+
 class Node:
    def __init__(self, val):
      self.val = val
