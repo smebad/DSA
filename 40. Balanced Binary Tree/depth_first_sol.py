3888a3c851fd8b77327743170cb4c8ffbb3cdb3f
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-17 21:33:31 +0500

    Depth First Solution for the problem

---
+++ 40. Balanced Binary Tree/depth_first_sol.py
@@ -0,0 +1,75 @@
+# Balanced Binary Tree
+# Given a binary tree, determine if it is height-balanced.
+
+ 
+
+# Example 1:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: true
+
+# Example 2:
+# Input: root = [1,2,2,3,3,null,null,4,4]
+# Output: false
+
+# Example 3:
+# Input: root = []
+# Output: true
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 5000].
+# -104 <= Node.val <= 104
+
+
+# Brute Force Solution:
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+    
+# Time Complexity: O(n), where n is the number of nodes in the tree. We visit each node once. This is because we are using a depth-first search (DFS) approach to traverse the tree. In the worst case, the tree is skewed, and we have to visit all nodes for each node.
+# Space Complexity: O(h), where h is the height of the tree. This is the space used by the recursion stack. In the worst case, the height of the tree can be n, leading to O(n) space complexity.
+# However this solution is more optimal than the brute force solution as it calculates the height of the tree in a single traversal, making it O(n) in time complexity.
+
+
+# Test Cases:
+
+# Test Case 1:
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+print(Solution().isBalanced(root)) # True
+
+# Test Case 2:
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(3)
+root.left.left.left = TreeNode(4)
+root.left.left.right = TreeNode(4)
+
+print(Solution().isBalanced(root)) # False
+
+# Test Case 3:
+root = None
+print(Solution().isBalanced(root)) # True
