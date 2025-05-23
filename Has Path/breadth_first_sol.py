2deab22ef612405f1a6774dc8eadeab698edd7df
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-23 21:46:08 +0500

    extra comments removed

--- Has Path/breadth_first_sol.py
+++ Has Path/breadth_first_sol.py
@@ -1,7 +1,6 @@
 # has path
-# Write a function, has_path, that takes in a dictionary representing the adjacency list of a directed acyclic graph and two nodes (src, dst). The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
-
 # Breadth First Solution:
+
 from collections import deque
 
 def has_path(graph, src, dst):
