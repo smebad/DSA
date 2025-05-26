af9987e08c2fd7bfd567a67c71365c2652ac87dc
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-26 19:51:57 +0500

    brute force solution for the problem

--- 48. Kth Smallest Element in a BST/brute_force_sol.py
+++ 48. Kth Smallest Element in a BST/brute_force_sol.py
@@ -1,30 +1,7 @@
 # Kth Smallest Element in a BST
-# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+# Brute Force Solution:
 
- 
-
-# Example 1:
-
-
-# Input: root = [3,1,4,null,2], k = 1
-# Output: 1
-# Example 2:
-
-
-# Input: root = [5,3,6,2,4,null,null,1], k = 3
-# Output: 3
- 
-
-# Constraints:
-
-# The number of nodes in the tree is n.
-# 1 <= k <= n <= 104
-# 0 <= Node.val <= 104
-
-
-# Iterative DFS Solution:
 from typing import Optional
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
