5d1820df7d74d72ae54965dba486bacd4aec630c
Ebad <146643721+smebad@users.noreply.github.com>
2025-05-29 18:45:20 +0500

    Heap (optimal) solution for the DSA problem

--- 51. Last Stone Weight/heap_sol.py
+++ 51. Last Stone Weight/heap_sol.py
@@ -1,28 +1,6 @@
 # Last Stone Weight
-
-# Example 1:
-
-# Input: stones = [2,7,4,1,8,1]
-# Output: 1
-# Explanation: 
-# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
-# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
-# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
-# we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
-
-# Example 2:
-
-# Input: stones = [1]
-# Output: 1
- 
-
-# Constraints:
-
-# 1 <= stones.length <= 30
-# 1 <= stones[i] <= 1000
-
-
 # Heap solution:
+
 import heapq
 from typing import List
 
