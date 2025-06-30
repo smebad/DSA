1928c0e26d7031ee8de6da7aa8a20c1465ca9489
Ebad <146643721+smebad@users.noreply.github.com>
2025-06-30 18:39:23 +0500

    Binary Search solution for the dsa problem

---
+++ 80. Sqrt(x)/binary_search_sol.py
@@ -0,0 +1,57 @@
+# Sqrt(x)
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+# You must not use any built-in exponent function or operator.
+
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+# Example 2:
+
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+# Constraints:
+
+# 0 <= x <= 231 - 1
+
+
+# Binary Search Solution:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        res = 0
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if m * m > x:
+                r = m - 1
+            elif m * m < x:
+                l = m + 1
+                res = m
+            else:
+                return m
+
+        return res
+    
+# TIme Complexity: O(log n) where n is the input number x. The binary search algorithm halves the search space on each iteration, leading to a logarithmic time complexity.
+# Space Complexity: O(1) as we are not using any additional data structures.
+# The algorithm uses a constant amount of space for variables like l, r, m, and res.
+# This is efficient for the problem constraints, which allow x to be as large as 2^31 - 1.
+
+
+# Test Cases
+# Test Case 1:
+x = 4
+print(Solution().mySqrt(x))  # Output: 2
+
+# Test Case 2:
+x = 8
+print(Solution().mySqrt(x))  # Output: 2
