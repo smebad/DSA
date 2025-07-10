12b46c378b1fa81049aba5a6034662fb8680bbf1
Ebad <146643721+smebad@users.noreply.github.com>
2025-07-10 16:19:16 +0500

    Updated Iterative Solution for the problem

--- 88. Insert into a Binary Search Tree/iterative_sol.py
+++ 88. Insert into a Binary Search Tree/iterative_sol.py
@@ -1,35 +1,4 @@
 # Insert into a Binary Search Tree
-# You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
-
-# Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
-
- 
-
-# Example 1:
-
-
-# Input: root = [4,2,7,1,3], val = 5
-# Output: [4,2,7,1,3,5]
-# Explanation: Another accepted tree is:
-
-# Example 2:
-
-# Input: root = [40,20,60,10,30,50,70], val = 25
-# Output: [40,20,60,10,30,50,70,null,null,25]
-# Example 3:
-
-# Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
-# Output: [4,2,7,1,3,5]
- 
-
-# Constraints:
-
-# The number of nodes in the tree will be in the range [0, 104].
-# -108 <= Node.val <= 108
-# All the values Node.val are unique.
-# -108 <= val <= 108
-# It's guaranteed that val does not exist in the original BST.
-
 
 # Iterative Solution:
 from typing import Optional, List
@@ -44,15 +13,21 @@ class Solution:
         if not root:
             return TreeNode(val)
 
-        if val > root.val:
-            root.right = self.insertIntoBST(root.right, val)
-        else:
-            root.left = self.insertIntoBST(root.left, val)
-
-        return root
+        cur = root
+        while True:
+            if val > cur.val:
+                if not cur.right:
+                    cur.right = TreeNode(val)
+                    return root
+                cur = cur.right
+            else:
+                if not cur.left:
+                    cur.left = TreeNode(val)
+                    return root
+                cur = cur.left
     
-# Time Complexity: O(h) where h is the height of the tree. In the worst case, the tree is skewed, and we have to visit all nodes for each node.
-# Space Complexity: O(1) where h is the height of the tree. This is the space used by the recursion stack.
+# Time Complexity: O(h) where h is the height of the tree. In the worst case, the tree is skewed, and we have to visit all nodes for each node. This is because we are using a breadth-first search (BFS) approach to traverse the tree.
+# Space Complexity: O(1) since we are using a constant amount of space for the current node and the queue.
 # This iterative solution is optimal in terms of time and space complexity.
 
 # Test Cases
