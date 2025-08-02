b74387aca9f3d3fb212eae57ce7e8d2e45a82d79
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-02 16:45:40 +0500

    recursive solution for the problem ( comments removed)

--- 107. Is Subsequence/recursion_sol.py
+++ 107. Is Subsequence/recursion_sol.py
@@ -1,31 +1,6 @@
 # Is Subsequence
-# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
-
-# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
-
- 
-
-# Example 1:
-
-# Input: s = "abc", t = "ahbgdc"
-# Output: true
-# Example 2:
-
-# Input: s = "axc", t = "ahbgdc"
-# Output: false
- 
-
-# Constraints:
-
-# 0 <= s.length <= 100
-# 0 <= t.length <= 104
-# s and t consist only of lowercase English letters.
- 
-
-# Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
-
-
 # Recursive Solution:
+
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         def rec(i, j):
@@ -54,4 +29,5 @@ print(sol.isSubsequence(s1, t1))  # Expected output: True
 # Test Case 2:
 s2 = "axc"
 t2 = "ahbgdc"
+
 print(sol.isSubsequence(s2, t2))  # Expected output: False
