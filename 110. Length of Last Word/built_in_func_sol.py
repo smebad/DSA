b93b40cb598801b54f7bc9bd58b70bb697ec3ca1
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-05 16:14:00 +0500

    built-in python function solution

---
+++ 110. Length of Last Word/built_in_func_sol.py
@@ -0,0 +1,56 @@
+# Length of Last Word
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+
+# Built-in function Solution:
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split().pop())
+    
+# Time Complexity: O(n), where n is the length of the string s. The split operation traverses the string to create a list of words, and pop retrieves the last word.
+# Space Complexity: O(n), as the split operation creates a list of words, which can take up space proportional to the number of words in the string.
+# This solution uses Python's built-in string methods to split the string into words and then retrieves the last word's length. However, it is not recommended to use this method for interview scenarios where efficiency is crucial, as it creates an intermediate list of words.
+
+
+# Test Cases
+# Test Case 1:
+s1 = "Hello World"
+solution1 = Solution()
+print(solution1.lengthOfLastWord(s1))  # Output: 5
+
+# Test Case 2:
+s2 = "   fly me   to   the moon  "
+solution2 = Solution()
+print(solution2.lengthOfLastWord(s2))  # Output: 4
+
+# Test Case 3:
+s3 = "luffy is still joyboy"
+solution3 = Solution()
+print(solution3.lengthOfLastWord(s3))  # Output: 6
