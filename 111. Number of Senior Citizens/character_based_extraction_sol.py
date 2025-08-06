3377138ae73de1d2978a784e8d449108f0017f33
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-06 16:52:50 +0500

    character based extraction solution

---
+++ 111. Number of Senior Citizens/character_based_extraction_sol.py
@@ -0,0 +1,55 @@
+# Number of Senior Citizens
+# You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+# The first ten characters consist of the phone number of passengers.
+# The next character denotes the gender of the person.
+# The following two characters are used to indicate the age of the person.
+# The last two characters determine the seat allotted to that person.
+# Return the number of passengers who are strictly more than 60 years old.
+
+ 
+
+# Example 1:
+
+# Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+# Output: 2
+# Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+# Example 2:
+
+# Input: details = ["1313579440F2036","2921522980M5644"]
+# Output: 0
+# Explanation: None of the passengers are older than 60.
+ 
+
+# Constraints:
+
+# 1 <= details.length <= 100
+# details[i].length == 15
+# details[i] consists of digits from '0' to '9'.
+# details[i][10] is either 'M' or 'F' or 'O'.
+# The phone numbers and seat numbers of the passengers are distinct.
+
+
+# Character Based Solution:
+from typing import List
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        res = 0
+        for d in details:
+            ten = ord(d[11]) - ord("0")
+            one = ord(d[12]) - ord("0")
+            age = one + 10 * ten
+            if age > 60:
+                res += 1
+        return res
+    
+# Time Complexity: O(n), where n is the number of passengers in the details list. We iterate through each passenger's details once. In each iteration, we extract the age from the details string using character-based operations, which takes O(1) time.
+# Space Complexity: O(1), as we are using a constant amount of space for the result variable.
+# This solution efficiently counts the number of senior citizens by checking the age substring in each passenger's details string using character-based operations. It avoids unnecessary string operations and directly accesses the relevant characters for age comparison.
+
+# Test Cases
+# Test Case 1:
+print(Solution().countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"])) # 2
+
+# Test Case 2:
+print(Solution().countSeniors(["1313579440F2036","2921522980M5644"])) # 0
