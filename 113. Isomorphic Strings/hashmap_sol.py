ef4c25ff034a3e564b199eaf619c6859963e802f
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-09 22:21:34 +0500

    Hash-map solution with test cases

--- 113. Isomorphic Strings/hashmap_sol.py
+++ 113. Isomorphic Strings/hashmap_sol.py
@@ -1,49 +1,6 @@
 # Isomorphic Strings
-# Given two strings s and t, determine if they are isomorphic.
-
-# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
- 
-
-# Example 1:
-
-# Input: s = "egg", t = "add"
-
-# Output: true
-
-# Explanation:
-
-# The strings s and t can be made identical by:
-
-# Mapping 'e' to 'a'.
-# Mapping 'g' to 'd'.
-# Example 2:
-
-# Input: s = "foo", t = "bar"
-
-# Output: false
-
-# Explanation:
-
-# The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
-
-# Example 3:
-
-# Input: s = "paper", t = "title"
-
-# Output: true
-
- 
-
-# Constraints:
-
-# 1 <= s.length <= 5 * 104
-# t.length == s.length
-# s and t consist of any valid ascii character.
-
 # Hashmap solution:
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mapST, mapTS = {}, {}
@@ -73,3 +30,9 @@ print(sol.isIsomorphic(s1, t1))  # Expected output: True
 s2 = "foo"
 t2 = "bar"
 print(sol.isIsomorphic(s2, t2))  # Expected output: False
+
+# Test Case 3:
+s3 = "paper"
+t3 = "title"
+print(sol.isIsomorphic(s3, t3))  # Expected output: True
+
