553d53ef8d54f76687ec3fac7e0601ea03f70403
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-10 22:46:02 +0500

    Add: Iterative solution for the problem

--- 114. Can Place Flowers/iteration_sol.py
+++ 114. Can Place Flowers/iteration_sol.py
@@ -1,31 +1,4 @@
 # Can Place Flowers
-# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
-
-# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
-
- 
-
-# Example 1:
-# Input: flowerbed = [1,0,0,0,1], n = 1
-# Output: true
-
-# Example 2:
-# Input: flowerbed = [1,0,0,0,1], n = 2
-# Output: false
- 
-
-# Constraints:
-
-# 1 <= flowerbed.length <= 2 * 104
-# flowerbed[i] is 0 or 1.
-# There are no two adjacent flowers in flowerbed.
-# 0 <= n <= flowerbed.length
-# Seen this question in a real interview before?
-# 1/5
-# Yes
-# No
-
-
 # Iteration Solution:
 from typing import List
 
@@ -55,3 +28,4 @@ print(solution.canPlaceFlowers(flowerbed1, n1))  # Output: True
 flowerbed2 = [1, 0, 0, 0, 1]
 n2 = 2
 print(solution.canPlaceFlowers(flowerbed2, n2))  # Output: False
+
