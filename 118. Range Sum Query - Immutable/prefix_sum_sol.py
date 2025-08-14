2f4d239fe73a87cf95aa098526cb0825ee345758
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-14 16:59:12 +0500

    prefix sum (optimal) solution

--- 118. Range Sum Query - Immutable/prefix_sum_sol.py
+++ 118. Range Sum Query - Immutable/prefix_sum_sol.py
@@ -1,36 +1,4 @@
 # Range Sum Query - Immutable
-# Given an integer array nums, handle multiple queries of the following type:
-
-# Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
-# Implement the NumArray class:
-
-# NumArray(int[] nums) Initializes the object with the integer array nums.
-# int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
- 
-
-# Example 1:
-
-# Input
-# ["NumArray", "sumRange", "sumRange", "sumRange"]
-# [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
-# Output
-# [null, 1, -1, -3]
-
-# Explanation
-# NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
-# numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
-# numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
-# numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
- 
-
-# Constraints:
-
-# 1 <= nums.length <= 104
-# -105 <= nums[i] <= 105
-# 0 <= left <= right < nums.length
-# At most 104 calls will be made to sumRange.
-
-
 # Prefix-Sum Solution:
 class NumArray:
     def __init__(self, nums):
@@ -59,4 +27,5 @@ print(numArray.sumRange(0, 2))  # Output: 1
 print(numArray.sumRange(2, 5))  # Output: -1
 
 # Test Case 3:
+
 print(numArray.sumRange(0, 5))  # Output: -3
