fc49de323b24b572d9a66bdf3124226daba49b8e
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-17 23:04:42 +0500

    negative-marking solution for the problem

--- 119. Find All Numbers Disappeared in an Array/negative_marking_sol.py
+++ 119. Find All Numbers Disappeared in an Array/negative_marking_sol.py
@@ -1,28 +1,4 @@
 # Find All Numbers Disappeared in an Array
-# Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
-
- 
-
-# Example 1:
-
-# Input: nums = [4,3,2,7,8,2,3,1]
-# Output: [5,6]
-# Example 2:
-
-# Input: nums = [1,1]
-# Output: [2]
- 
-
-# Constraints:
-
-# n == nums.length
-# 1 <= n <= 105
-# 1 <= nums[i] <= n
- 
-
-# Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
-
-
 # Negative-Markings Solution:
 from typing import List
 
@@ -54,4 +30,5 @@ print(output1)  # Expected Output: [5, 6]
 nums2 = [1, 1]
 solution2 = Solution()
 output2 = solution2.findDisappearedNumbers(nums2)
+
 print(output2)  # Expected Output: [2]
