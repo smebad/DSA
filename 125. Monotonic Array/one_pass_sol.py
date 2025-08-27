31a11c30e051f4fb1d8fa8e20de82f95c5934283
Ebad <146643721+smebad@users.noreply.github.com>
2025-08-27 22:21:05 +0500

    One Pass Solution for the problem

--- 125. Monotonic Array/one_pass_sol.py
+++ 125. Monotonic Array/one_pass_sol.py
@@ -1,32 +1,4 @@
 # Monotonic Array
-# An array is monotonic if it is either monotone increasing or monotone decreasing.
-
-# An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
-
-# Given an integer array nums, return true if the given array is monotonic, or false otherwise.
-
- 
-
-# Example 1:
-
-# Input: nums = [1,2,2,3]
-# Output: true
-# Example 2:
-
-# Input: nums = [6,5,4,4]
-# Output: true
-# Example 3:
-
-# Input: nums = [1,3,2]
-# Output: false
- 
-
-# Constraints:
-
-# 1 <= nums.length <= 105
-# -105 <= nums[i] <= 105
-
-
 # One Pass Solution:
 from typing import List
 
@@ -48,6 +20,7 @@ class Solution:
 
 # Test Cases
 sol = Solution()
+
 # Test Case 1:
 nums1 = [1, 2, 2, 3]
 print(sol.isMonotonic(nums1))  # Output: True
