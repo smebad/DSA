c00acea5260906c236d914ff07151b3dbb104817
Ebad <146643721+smebad@users.noreply.github.com>
2025-11-03 22:30:50 +0500

    hashmap solution

---
+++ 183. Repeated DNA Sequences/hashmap_sol.py
@@ -0,0 +1,55 @@
+# Repeated DNA Sequences
+# The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+# For example, "ACGAATTCCG" is a DNA sequence.
+# When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+# Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+# Output: ["AAAAACCCCC","CCCCCAAAAA"]
+# Example 2:
+
+# Input: s = "AAAAAAAAAAAAA"
+# Output: ["AAAAAAAAAA"]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s[i] is either 'A', 'C', 'G', or 'T'.
+
+# Hash Map Solution:
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) < 10:
+            return []
+
+        mp = defaultdict(int)
+        res = []
+        for l in range(len(s) - 9):
+            cur = s[l: l + 10]
+            mp[cur] += 1
+            if mp[cur] == 2:
+                res.append(cur)
+
+        return res
+
+
+# Test Cases:
+sol = Solution()
+
+# Test Case 1:
+s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+print(sol.findRepeatedDnaSequences(s))  # Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+
+# Test Case 2:
+s = "AAAAAAAAAAAAA"
+print(sol.findRepeatedDnaSequences(s))  # Output: ["AAAAAAAAAA"]
