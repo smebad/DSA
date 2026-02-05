46166444ce2e4fbe4250525cc3398048d50c2502
Ebad <146643721+smebad@users.noreply.github.com>
2026-02-05 23:32:36 +0500

    depth-first-iterative-sol.py

--- Tree Min Value/depth-first-iterative-sol.py
+++ Tree Min Value/depth-first-iterative-sol.py
@@ -1,9 +1,4 @@
-# tree min value
-# Write a function, tree_min_value, that takes in the root of a binary tree that contains number values. The function should return the minimum value within the tree.
-
-# You may assume that the input tree is non-empty.
-
-
+# Tree min value
 # Depth First Solution (Iterative):
 class Node:
    def __init__(self, val):
