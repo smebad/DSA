171843462af9818c5b2ff1641e2b55eb950f894c
Ebad <146643721+smebad@users.noreply.github.com>
2026-02-15 22:36:17 +0500

    iterative solution

--- Zipper Lists/iterative_sol.py
+++ Zipper Lists/iterative_sol.py
@@ -1,12 +1,5 @@
-# zipper lists
-# Write a function, zipper_lists, that takes in the head of two linked lists as arguments. The function should zipper the two lists together into single linked list by alternating nodes. If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. The function should return the head of the zippered linked list.
-
-# Do this in-place, by mutating the original Nodes.
-
-# You may assume that both input lists are non-empty.
-
-
-# Iterative Solution
+# Zipper lists
+# Iterative solution
 
 class Node:
    def __init__(self, val):
@@ -131,7 +124,6 @@ two.next = three
 
 w = Node("w")
 # w
-
 print(zipper_lists(one, w))
 # 1 -> w -> 2 -> 3
 
