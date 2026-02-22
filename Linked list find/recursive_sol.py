26868943777d7d368ad7da6eb7915d34a9d8d65b
Ebad <146643721+smebad@users.noreply.github.com>
2026-02-22 23:08:54 +0500

    recursive sol

--- Linked list find/recursive_sol.py
+++ Linked list find/recursive_sol.py
@@ -1,6 +1,4 @@
 # linked list find
-# Write a function, linked_list_find, that takes in the head of a linked list and a target value. The function should return a boolean indicating whether or not the linked list contains the target.
-
 # recursive Solution
 
 class Node:
