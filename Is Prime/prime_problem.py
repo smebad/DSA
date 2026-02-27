5ebfc1c081d77419916b4138259ecbcbdc4b72d9
Ebad <146643721+smebad@users.noreply.github.com>
2026-02-27 22:11:20 +0500

    prime problem dsa solution

--- Is Prime/prime_problem.py
+++ Is Prime/prime_problem.py
@@ -1,13 +1,4 @@
 # Is Prime
-
-# Write a function, is_prime, that takes in a number as an argument. The function should return a boolean indicating whether or not the given number is prime.
-
-# A prime number is a number that is only divisible by two distinct numbers: 1 and itself.
-
-# For example, 7 is a prime because it is only divisible by 1 and 7. For example, 6 is not a prime because it is divisible by 1, 2, 3, and 6.
-
-#You can assume that the input number is a positive integer.
-
 def is_prime(num): 
   if num < 2:
      return False # Step 1: Checking if the number is less than 2
